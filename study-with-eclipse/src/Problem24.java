81598f4085419142c8d6add4ea5a1b771639e656
Ahhhhhhyeong <dkdud081@gmail.com>
2022-11-15 20:43:50 +0900

    24 solved

---
+++ study-with-eclipse/src/Problem24.java
@@ -0,0 +1,42 @@
+/**
+ * ∏‡≈‰∏µ
+ */
+import java.util.Scanner;
+  
+public class Problem24 {
+  public static void main(String[] args){
+    Scanner in=new Scanner(System.in);
+    int n = in.nextInt();
+    int m = in.nextInt();
+    int[][] arr = new int[m][n];
+    for(int i=0; i<m; i++) {
+    	for(int j=0; j<n; j++) {
+    		arr[i][j] = in.nextInt();
+    	}
+    }
+    System.out.println(solution(n,m,arr));
+    return ;
+  }
+  
+  public static int solution(int n, int m, int[][] arr) {
+	  int result = 0;
+	  
+	  for(int i=1; i<=n; i++) {
+		  for(int j=1; j<=n; j++) {
+			  int cnt = 0;
+			  for(int k=0; k<m; k++) {
+				  int pi=0, pj=0;
+				  for(int s=0; s<n; s++) {
+					  if(arr[k][s] == i) pi = s;
+					  if(arr[k][s] == j) pj = s;
+				  }
+				  if(pi < pj) cnt++;
+			  }
+			  if(cnt==m) {
+				  result++;
+			  }
+		  }
+	  }	  
+	  return result;
+  }
+}
