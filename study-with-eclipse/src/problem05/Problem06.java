846f7da1a6cc2810c7fc10f6daa57caf826ec46f
Ahhhhhhyeong <dkdud081@gmail.com>
2022-11-16 21:56:34 +0900

    05-06 solved

---
+++ study-with-eclipse/src/problem05/Problem06.java
@@ -0,0 +1,31 @@
+package problem05;
+/**
+ * 공주 구하기
+ */
+import java.util.*;
+
+public class Problem06 {
+  public static void main(String[] args){
+    Scanner in=new Scanner(System.in);
+    int n = in.nextInt();
+    int k = in.nextInt();
+    System.out.println(solution(n, k));
+    return ;
+  }
+  
+  private static int solution(int n, int k) {
+	int result = 0;
+	Queue<Integer> que = new LinkedList<>();
+	for(int i=1; i<=n; i++) {
+		que.offer(i);
+	}
+	while(!que.isEmpty()) {
+		for(int i=1; i<k; i++) que.offer(que.poll());
+		que.poll();
+		if(que.size() == 1) result = que.poll();
+	}	
+	return result;
+  }
+
+
+}
