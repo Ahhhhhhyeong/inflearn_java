c4d4a7bf8a7be0e5e21a245f49b609507234a4fe
Ahhhhhhyeong <dkdud081@gmail.com>
2022-11-16 22:36:03 +0900

    05-07 update

--- study-with-eclipse/src/problem05/Problem07.java
+++ study-with-eclipse/src/problem05/Problem07.java
@@ -14,8 +14,7 @@ public class Problem07 {
   private static String Solution(String subject, String hclass) {
 	  String result = "YES";
 	  Queue<Character> que = new LinkedList<>();
-	  for(char x : hclass.toCharArray()) que.offer(x);
-	  
+	  for(char x : hclass.toCharArray()) que.offer(x);	  
 	  for(char x : subject.toCharArray()) {
 		  while(!que.isEmpty()) {
 			  char tmp = que.poll();
@@ -27,5 +26,18 @@ public class Problem07 {
 		  }
 	  }	  
 	  return result;		
+  }
+  
+  private static String Solution2(String need, String plan) { // 인프런 강의 풀이
+	  String answer = "YES";
+	  Queue<Character> que = new LinkedList<>();
+	  for(char x : need.toCharArray()) que.offer(x);
+	  for(char x : plan.toCharArray()) {
+		  if(que.contains(x)) {
+			  if(x!=que.poll()) return "NO";
+		  }
+	  }
+	  if(!que.isEmpty()) return "NO";	  
+	  return answer;
   }
 }
