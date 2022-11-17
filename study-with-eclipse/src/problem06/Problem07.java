720906b93ae562f5078682aa7e5d067181add118
Ahhhhhhyeong <dkdud081@gmail.com>
2022-11-17 16:29:40 +0900

    06-07 class 객체만들어서 하는방법 update

--- study-with-eclipse/src/problem06/Problem07.java
+++ study-with-eclipse/src/problem06/Problem07.java
@@ -3,18 +3,42 @@ package problem06;
 import java.util.*;
 
 
+class Point implements Comparable<Point>{
+	public int x, y;
+	Point(int x, int y){
+		this.x = x;
+		this.y = y;		
+	}
+	
+	@Override
+	public int compareTo(Point o) {
+		if(this.x==o.x) return this.y - o.y; //현재객체-새로온객체 => 오름차순
+		else return this.x - o.x;
+	}
+}
 public class Problem07 {
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
 		int n = in.nextInt();
-		int[][] arr = new int[n][2];
-		for(int i = 0; i<n; i++) {
-			for(int j=0; j<2; j++) {
-				arr[i][j] = in.nextInt();
-			}
+		
+		ArrayList<Point> arr = new ArrayList<>();
+		for(int i=0; i<n; i++) {
+			int x = in.nextInt();
+			int y = in.nextInt();
+			arr.add(new Point(x, y));
 		}
-		solution(n, arr);
+		Collections.sort(arr);
+		for(Point o : arr) System.out.println(o.x + " " + o.y);
+		
+//		int[][] arr2 = new int[n][2];
+//		for(int i = 0; i<n; i++) {
+//			for(int j=0; j<2; j++) {
+//				arr2[i][j] = in.nextInt();
+//			}
+//		}
+//		solution(n, arr2);
 	}
+	
 	private static void solution(int n, int[][] arr) {
 		Arrays.sort(arr, new Comparator<int[]>() {
 			@Override
@@ -24,10 +48,7 @@ public class Problem07 {
 			}
 		});
 		for(int i = 0; i<n; i++) {
-			for(int j=0; j<2; j++) {
-				System.out.printf("%d ", arr[i][j]);
-			}
-			System.out.println();
+			System.out.println(arr[i][0] + " " + arr[i][1]);
 		}
 	}
 }
