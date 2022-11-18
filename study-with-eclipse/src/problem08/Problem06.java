cb0eae6873c624ae27e5d0cab1936e6e7830def5
Ahhhhhhyeong <dkdud081@gmail.com>
2022-11-18 17:23:20 +0900

    08-06 순열 구하기 fin

---
+++ study-with-eclipse/src/problem08/Problem06.java
@@ -0,0 +1,42 @@
+package problem08;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * 순열 구하기
+ *
+ */
+public class Problem06 {
+	static int[] pm, ch, arr;
+	static int n, m;
+	
+	private static void DFS(int L) {
+		if(L==m) {
+			for(int x : pm) System.out.print(x + " ");
+			System.out.println();
+		}
+		else {
+			for(int i=0; i<n; i++) {
+				if(ch[i] == 0) {
+					ch[i]=1;
+					pm[L] = arr[i];
+					DFS(L+1);
+					ch[i]=0;
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		n = in.nextInt();
+		m = in.nextInt();
+		arr = new int[n];
+		for(int i=0; i<n; i++) arr[i] = in.nextInt();
+		ch = new int[n];
+		pm = new int[m];
+		DFS(0);
+	}
+
+}
