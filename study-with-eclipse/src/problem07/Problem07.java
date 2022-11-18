3a6c97184259773dffd761ca9d13995652fa6596
Ahhhhhhyeong <dkdud081@gmail.com>
2022-11-18 10:40:54 +0900

    07-07 update

--- study-with-eclipse/src/problem07/Problem07.java
+++ study-with-eclipse/src/problem07/Problem07.java
@@ -8,25 +8,25 @@ import java.util.Queue;
  * 이진트리 순회(BFS 넓이우선탐색: 레벨탐색)
  *
  */
-class Node {
+class Location {
 	int data;
-	Node lt,  rt;
-	public Node(int val) {
+	Location lt,  rt;
+	public Location(int val) {
 		data = val;
 		lt = rt = null;
 	}
 }
 public class Problem07 {
-	static Node root;
-	public static void BFS(Node root) {
-		Queue<Node> Q = new LinkedList<>();
+	static Location root;
+	public static void BFS(Location root) {
+		Queue<Location> Q = new LinkedList<>();
 		Q.offer(root);
 		int L = 0;
 		while(!Q.isEmpty()) {
 			int len = Q.size();
 			System.out.print(L + " : ");
 			for(int i=0; i<len; i++) {
-				Node cur = Q.poll();
+				Location cur = Q.poll();
 				System.out.print(cur.data + " ");
 				if(cur.lt!=null) Q.offer(cur.lt);
 				if(cur.rt!=null) Q.offer(cur.rt);
@@ -36,13 +36,13 @@ public class Problem07 {
 		}
 	}
 	public static void main(String[] args) {
-		root = new Node(1);
-		root.lt = new Node(2);
-		root.rt = new Node(3);
-		root.lt.lt = new Node(4);
-		root.lt.rt = new Node(5);
-		root.rt.lt = new Node(6);
-		root.rt.rt = new Node(7);
+		root = new Location(1);
+		root.lt = new Location(2);
+		root.rt = new Location(3);
+		root.lt.lt = new Location(4);
+		root.lt.rt = new Location(5);
+		root.rt.lt = new Location(6);
+		root.rt.rt = new Location(7);
 		BFS(root);
 
 	}
