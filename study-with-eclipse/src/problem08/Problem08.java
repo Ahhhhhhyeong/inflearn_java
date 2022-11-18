efc6a01320a340b225331a7614a5f3d002f0e42e
Ahhhhhhyeong <dkdud081@gmail.com>
2022-11-18 23:15:30 +0900

    08-08 수열 추측하기

--- study-with-eclipse/src/problem08/Problem08.java
+++ study-with-eclipse/src/problem08/Problem08.java
@@ -1,16 +1,52 @@
 package problem08;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Problem08 {
+	static int[] b, p, ch;
+	static int n, f;
+	boolean flag = false;
+	int[][] dy = new int[35][35];
 
-	public static void main(String[] args) {
-		Scanner in=new Scanner(System.in);
-	    int input1 = in.nextInt();
-	    int input2 = in.nextInt();
-	    System.out.println(input1 + input2);
-	    return ;
-
+	public int combi(int n, int r) {
+		if (dy[n][r] > 0)
+			return dy[n][r];
+		if (n == r || r == 0)
+			return 1;
+		else
+			return dy[n][r] = combi(n - 1, r - 1) + combi(n - 1, r);
 	}
 
+	public void DFS(int L, int sum) {
+		if (flag) return;
+		if (L == n) {
+			if (sum == f) {
+				for (int x : p) System.out.print(x + " ");
+				flag = true;
+			}
+		} else {
+			for (int i = 1; i <= n; i++) { // 1~n±îÁö
+				if (ch[i] == 0) {
+					ch[i] = 1;
+					p[L] = i;
+					DFS(L + 1, sum + (p[L] * b[L]));
+					ch[i] = 0;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Problem08 T = new Problem08();
+		Scanner kb = new Scanner(System.in);
+		n = kb.nextInt();
+		f = kb.nextInt();
+		b = new int[n];
+		p = new int[n];
+		ch = new int[n + 1];
+		for (int i = 0; i < n; i++) {
+			b[i] = T.combi(n - 1, i);
+		}
+		T.DFS(0, 0);
+	}
 }
