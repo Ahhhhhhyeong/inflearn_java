cb70bfbb7b3c810e52370531756be7ed549593ff
Ahhhhhhyeong <dkdud081@gmail.com>
2022-11-18 18:32:56 +0900

    백업

---
+++ study-with-eclipse/src/problem08/Problem08.java
@@ -0,0 +1,16 @@
+package problem08;
+
+import java.util.Scanner;
+
+public class Problem08 {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+	    int input1 = in.nextInt();
+	    int input2 = in.nextInt();
+	    System.out.println(input1 + input2);
+	    return ;
+
+	}
+
+}
