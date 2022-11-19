1e97b69a812555b46907bdd45bd54679e1434c65
Ahhhhhhyeong <dkdud081@gmail.com>
2022-11-19 16:11:34 +0900

    08-12 토마토...BFS활용

---
+++ study-with-eclipse/src/problem08/Problem13.java
@@ -0,0 +1,14 @@
+package problem08;
+/**
+ * 섬나라 아일랜드
+ */
+import java.util.*;
+
+public class Problem13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
