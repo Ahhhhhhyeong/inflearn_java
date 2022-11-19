dc5877863cc895632bfaf7ff6707b0f46f130347
Ahhhhhhyeong <dkdud081@gmail.com>
2022-11-19 23:38:39 +0900

    원더랜드

--- study-with-eclipse/src/problem09/Problem07.java
+++ study-with-eclipse/src/problem09/Problem07.java
@@ -5,61 +5,52 @@ package problem09;
  *
  */
 import java.util.*;
-
-class Edge2 implements Comparable<Edge2>{
-	public int v1;
-	public int v2;
+class Land implements Comparable<Land>{
+	public int vex;
 	public int cost;
-	Edge2(int v1, int v2, int cost){
-		this.v1 = v1;
-		this.v2 = v2;
+	Land(int vex, int cost){
+		this.vex = vex;
 		this.cost = cost;
 	}
 	@Override
-	public int compareTo(Edge2 o) {
+	public int compareTo(Land o) {
 		return this.cost-o.cost;
 	}
 }
 
 public class Problem07 {
-	static int[] unf;
-	
-	public static int Find(int a) {
-		if(a == unf[a]) return a;
-		else return unf[a] = Find(unf[a]);
-	}
-	
-	public static void Union(int a, int b) {
-		int fa = Find(a);
-		int fb = Find(b);
-		if(fa != fb) unf[fa] = fb;
-	}
-	
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
 		int v = in.nextInt();
 		int e = in.nextInt();
-		unf = new int[v+1];
-		ArrayList<Edge2> arr = new ArrayList<>();
-		for(int i=1; i<=v; i++) unf[i]=i;
+		ArrayList<ArrayList<Land>> graph = new ArrayList<ArrayList<Land>>();
+		for(int i=0; i<=v; i++) {
+			graph.add(new ArrayList<Land>());
+		}
+		int[] ch = new int[v+1];
 		for(int i=0; i<e; i++) {
 			int a = in.nextInt();
 			int b = in.nextInt();
 			int c = in.nextInt();
-			arr.add(new Edge2(a, b, c));
-		}
-		int answer =0, cnt=0;
-		Collections.sort(arr);
-		for(Edge2 ob : arr) {
-			int fv1=Find(ob.v1);
-			int fv2=Find(ob.v2);
-			if(cnt == v-1) break;
-			if(fv1 != fv2) { 
-				answer+=ob.cost;
-				cnt++;
-				Union(ob.v1, ob.v2);
+			graph.get(a).add(new Land(b,c)); // 양방향이라 a경우랑 b경우 다 넣어줌
+			graph.get(b).add(new Land(a,c));
+		}	
+		int answer = 0;
+		PriorityQueue<Land> pQ = new PriorityQueue<>();
+		pQ.offer(new Land(1, 0));
+		while(!pQ.isEmpty()) {
+			Land tmp = pQ.poll();
+			int ev = tmp.vex; // 도착 정점
+			if(ch[ev] == 0) { // 정점이 체크가 되어있지 않는 경우 
+				ch[ev] = 1; // 체크!
+				answer += tmp.cost; // 비용을 더해줌
+				for(Land ob : graph.get(ev)) { // 연결된 정보 탐색
+					// 연결된 정점이 있을 경우,
+					// pQ에 정점과 비용을 넣어줌
+					if(ch[ob.vex] == 0) pQ.offer(new Land(ob.vex, ob.cost)); 
+				}
 			}
-		}
+		}		
 		System.out.println(answer);
 	}
 
